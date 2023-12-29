35647bf06c8318e8b90c1f61dedff8cf808f407e
coihsan <110887888+coihsan@users.noreply.github.com>
2023-12-29 23:20:50 +0000

    Update 8 files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,27 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import astroRemark from "@astrojs/markdown-remark";
 import react from "@astrojs/react";
-import { remarkReadingTime } from './remark-reading-time.mjs';
+import { remarkReadingTime } from "./remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://example.com",
   integrations: [mdx(), sitemap(), tailwind(), react()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
   markdownOptions: {
     render: [
-        astroRemark,
-        {
-            rehypePlugins: [
-                "rehype-slug",
-                [
-                    "rehype-autolink-headings",
-                    { behavior: "append"},
-                ],
-                [
-                    "rehype-toc",
-                    { headings: ["h1", "h2"] }
-                ]
-            ],
-        },
+      astroRemark,
+      {
+        rehypePlugins: [
+          "rehype-slug",
+          ["rehype-autolink-headings", { behavior: "append" }],
+          ["rehype-toc", { headings: ["h1", "h2"] }],
+        ],
+      },
     ],
-},
+  },
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,19 @@
-import { defineCollection, reference, z } from 'astro:content';
-import { remarkReadingTime } from '../remark-reading-time.mjs';
-
+import { defineCollection, reference, z } from "astro:content";
 const blog = defineCollection({
-	type: 'content',
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.object({ 
-            src: z.string().optional().default(''),
-            alt: z.string().optional().default(''),
-        }).optional(),
-		tags: z.string()
-	}),
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z
+      .object({
+        src: z.string().optional().default(""),
+        alt: z.string().optional().default(""),
+      })
+      .optional(),
+    tags: z.string(),
+  }),
 });
 
 export const collections = { blog };
