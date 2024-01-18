651fac1e8d373fe4ab6dcdccbd1c24fe79f77592
coihsan <ihsan.onlinefs@gmail.com>
2024-01-18 22:29:54 +0700

    commit

---
+++ src/constants/tools.tsx
@@ -0,0 +1,16 @@
+interface typeTools{
+    title: string;
+    url: string;
+    categories: string;
+    image: string;
+    deskripsi: string;
+    rilisDate: string;
+}
+export const tools = {
+    title: "ChatGPT",
+    url: "#",
+    categories: "Productivity",
+    image: "#",
+    deskripsi: "Chat with AI for your daily work",
+    rilisDate: "Dec 02 2024",
+}
