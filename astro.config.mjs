8d2bb76b7ceb86d7c6dbaec4918ef31a20143ee4
coihsan <ihsan.onlinefs@gmail.com>
2024-03-22 01:50:46 +0700

    update and remove dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,61 +3,36 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import astroRemark from "@astrojs/markdown-remark";
-import preact from '@astrojs/preact';
-import react from "@astrojs/react";
 import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
-import markdownConfig from './src/lib/markdown.config'
-import AstroPWA from '@vite-pwa/astro';
-import webmanifest from 'astro-webmanifest';
+import markdownConfig from './src/lib/markdown.config';
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [
-    mdx(), 
-    sitemap(), 
-    tailwind(), 
-    react(), 
-    preact(), 
-    AstroPWA(),
-    webmanifest({
-      name: 'YourGrandpa Blog',
-      config: {
-        outfile: 'site.webmanifest',
-      },
-      // icon: 'src/images/your-icon.svg',
-      short_name: 'YourGrandpa App',
-      description: 'YourGrandpa documentation',
-      start_url: '/',
-      theme_color: '#3367D6',
-      background_color: '#000000',
-      display: 'standalone',
-    }),
-  ],
+  site: "https://yourgrandpa.vercel.app/",
+  integrations: [mdx(), sitemap(), tailwind()],
   build: {
     rollupOptions: {
-      external: ['sanitize-html'],
-    },
+      external: ['sanitize-html']
+    }
   },
   markdownOptions: {
     markdown: {
       remarkPlugins: [remarkReadingTime],
-      extendDefaultPlugins: true,
+      extendDefaultPlugins: true
     },
-    integrations: [
-      mdx({
-        ...markdownConfig,
-        extendPlugins: false,
-      }),
-    ],
-    render: [
-      astroRemark,
-      {
-        rehypePlugins: [
-          "rehype-slug",
-          ["rehype-autolink-headings", { behavior: "append" }],
-          ["rehype-toc", { headings: ["h1", "h2"] }],
-        ],
-      },
-    ],
+    integrations: [mdx({
+      ...markdownConfig,
+      extendPlugins: false
+    })],
+    render: [astroRemark, {
+      rehypePlugins: ["rehype-slug", ["rehype-autolink-headings", {
+        behavior: "append"
+      }], ["rehype-toc", {
+        headings: ["h1", "h2"]
+      }]]
+    }]
   },
+  output: "server",
+  adapter: vercel()
 });
